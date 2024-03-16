60aa12ec3f96f284dbb853d396a492fa44288c50
Cavlon <zmzf47@durham.ac.uk>
2024-03-16 22:28:18 +0000

    Basic Exemplar Inpainting

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import os
 import cv2
 import numpy as np
-import math
 
 count = 0
 def print_position(event, x, y, flags, param):
@@ -11,7 +10,199 @@ def print_position(event, x, y, flags, param):
         count += 1
         print(f"position ({x},{y})")
 
-def process(img):
+def exemplar_paint(img, ksize=9):
+    # Make image grey
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Initialise confidence values for each pixel
+    # Non-empty pixels as 1
+    # Empty as 0
+    # ret, confidence = cv2.threshold(img_grey, 20, 1, cv2.THRESH_BINARY)
+    confidence = np.zeros(img_grey.shape[:2], dtype="uint8")
+    confidence[img_grey < 20] = 255
+    kernel = np.ones((3,3),np.uint8)
+    confidence = cv2.dilate(confidence, kernel, iterations = 1)
+    confidence = 255 - confidence
+    confidence = confidence.astype('float64')
+    confidence /= 255
+    conf3C = cv2.cvtColor(confidence.astype('uint8'), cv2.COLOR_GRAY2BGR)
+    img *= conf3C
+    # cv2.imshow('Confidence', confidence)
+
+    k_area = ksize * ksize
+    halfk = ksize // 2
+
+    # Run until area is fully painted
+    while True:
+        # Convert image to grey
+        img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('Grey image', img_grey)
+
+        # Threshold to get mask of paint area
+        thresh = np.zeros(img_grey.shape[:2], dtype="uint8")
+        thresh[img_grey < 10] = 255
+        thresh = 255 - thresh
+
+        # cv2.imshow('Binary image', thresh)
+
+        # Check if there are still pixels to paint in
+        h, w = img.shape[:2]
+        sample = cv2.countNonZero(thresh)
+        if sample == (h * w):
+            break
+
+        # Find the fill front of the fill area
+        contour, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        contour = contour[1]
+
+        # img2 = img.copy()
+        # cv2.drawContours(image=img2, contours=contour, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+        # cv2.imshow('Contours', img2)
+
+        # Calculate the priority of each contour point
+        max_priority = None
+        chosen_point = None
+        c_val = None
+        for i in range(len(contour)):
+            point = contour[i][0]
+            c = 0   # Confidence
+            d = 0   # Data
+
+            # Calculate confidence for the contour point
+            # Iterate through the kernal
+            # Take the average of the confidence values
+            confidence_patch = confidence[point[1] - halfk: point[1] + halfk + 1, point[0] - halfk: point[0] + halfk + 1]
+            c = np.sum(confidence_patch) / k_area
+            # for j in range(ksize):
+            #     for k in range(ksize):
+            #         kpoint = ((point[0]-(ksize // 2)) + j, (point[1]-(ksize // 2)) + k)
+            #         if thresh[kpoint[1]][kpoint[0]] == 255:
+            #             c += confidence[kpoint[1]][kpoint[0]]
+            # c /= k_area
+
+            # Calculate the normal of the contour point
+            # Find the tangent
+            prev_i = (i - 1) % len(contour)
+            next_i = (i + 1) % len(contour)
+
+            prev_point = contour[prev_i][0]
+            next_point = contour[next_i][0]
+            tan = next_point - prev_point
+
+            # Find the vector orthogonal to the tangent
+            normal = [-tan[1], tan[0]]
+            normal = normal / np.linalg.norm(normal)
+
+            # Find the isothope of the contour point
+            # Calculate the image partial derivatives
+            part_dir_x = cv2.Sobel(img_grey, cv2.CV_64F, 1, 0, ksize=1)
+            part_dir_y = cv2.Sobel(img_grey, cv2.CV_64F, 0, 1, ksize=1)
+
+            # empty = np.zeros(img.shape[:2], dtype="uint8")
+            # part_dir = cv2.merge([empty, part_dir_x.astype('uint8'), part_dir_y.astype('uint8')])
+            # cv2.imshow('Partial Derivatives', part_dir)
+            # cv2.waitKey(0)
+
+            # Find the edge orientation and magnitude of each point using the partial derivatives
+            # edge_orient = math.atan(part_dir_y[point[1]][point[0]] / part_dir_x[point[1]][point[0]])
+            # edge_mag = math.sqrt((part_dir_x[point[1]][point[0]] ** 2) + (part_dir_y[point[1]][point[0]] ** 2))
+
+            # Use the edge magnitude and orientation to find the orthogonal isothope
+            # isothope = [math.cos(edge_orient) * edge_mag, math.sin(edge_orient) * edge_mag]
+            isothope = [-part_dir_x[point[1]][point[0]], -part_dir_y[point[1]][point[0]]]
+
+            # copy = cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR)
+            # cv2.line(copy, ((point[0], point[1])), ((point[0] + int(isothope[0] * 10), point[1] + int(isothope[1] * 10))), (0, 255, 0), 2, cv2.LINE_AA)
+            # cv2.line(copy, ((point[0], point[1])), ((point[0] + int(normal[0] * 10), point[1] + int(normal[1] * 10))), (0, 0, 255), 2, cv2.LINE_AA)
+            # cv2.imshow('Direction', copy)
+            # cv2.waitKey(0)
+
+            # Calculate the data value
+            d = ((normal[0] * isothope[0]) + (normal[1] * isothope[1])) / 255
+
+            priority = c * d
+
+            if max_priority == None:
+                max_priority = priority
+                chosen_point = point
+                c_val = c
+            
+            if priority > max_priority:
+                max_priority = priority
+                chosen_point = point
+                c_val = c
+
+        # print(chosen_point)
+
+        # img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+        fill_patch = img[chosen_point[1] - halfk: chosen_point[1] + halfk + 1, chosen_point[0] - halfk: chosen_point[0] + halfk + 1]
+        fill_patchmask = thresh[chosen_point[1] - halfk: chosen_point[1] + halfk + 1, chosen_point[0] - halfk: chosen_point[0] + halfk + 1] / 255
+        fill_patchmask = fill_patchmask.astype('uint8')
+        fill_patchmask3C = cv2.cvtColor(fill_patchmask, cv2.COLOR_GRAY2BGR)
+
+        # fill_view_patch1 = cv2.resize(fill_patch, (225, 225), interpolation = cv2.INTER_LINEAR)
+        # cv2.imshow('fill orig', fill_view_patch1)
+
+        min_ssd = None
+        sample_patch = None
+        for i in range(halfk, h - halfk + 1):
+            for j in range(halfk, w - halfk + 1):
+                check_patchmask = thresh[i - halfk: i + halfk + 1, j - halfk: j + halfk + 1]
+
+                if cv2.countNonZero(check_patchmask) != k_area:
+                    continue
+
+                check_patch = img[i - halfk: i + halfk + 1, j - halfk: j + halfk + 1]
+
+                diff = check_patch * fill_patchmask3C
+
+                # view_patch = cv2.cvtColor(diff, cv2.COLOR_LAB2BGR)
+                # view_patch = cv2.resize(diff, (225, 225), interpolation = cv2.INTER_LINEAR)
+                # b, g, r = cv2.split(view_patch)
+                # cv2.imshow('sample', view_patch)
+
+                diff = fill_patch - diff
+                diff = np.square(diff)
+                ssd = np.sum(diff)
+
+                # cv2.waitKey(0)
+
+                if min_ssd == None:
+                    min_ssd = ssd
+                    sample_patch = check_patch
+                    continue
+
+                if ssd < min_ssd:
+                    min_ssd = ssd
+                    sample_patch = check_patch
+        # print(min_ssd)
+        # sample_patch = cv2.cvtColor(sample_patch, cv2.COLOR_LAB2BGR)
+        # sample_view_patch = cv2.resize(sample_patch, (225, 225), interpolation = cv2.INTER_LINEAR)
+        # cv2.imshow('sample', sample_view_patch)
+        # cv2.waitKey(0)
+
+        fill_patchmask = 1 - fill_patchmask
+        fill_patchmask3C = cv2.cvtColor(fill_patchmask, cv2.COLOR_GRAY2BGR)
+        sample_patch = sample_patch * fill_patchmask3C
+        fill_patch = fill_patch + sample_patch
+
+        # fill_view_patch = cv2.resize(fill_patch, (225, 225), interpolation = cv2.INTER_LINEAR)
+        # cv2.imshow('fill', fill_view_patch)
+
+        img[chosen_point[1] - halfk: chosen_point[1] + halfk + 1, chosen_point[0] - halfk: chosen_point[0] + halfk + 1] = fill_patch
+
+        conf_upd = fill_patchmask.astype('float64') * c_val
+        confidence[chosen_point[1] - halfk: chosen_point[1] + halfk + 1, chosen_point[0] - halfk: chosen_point[0] + halfk + 1] += conf_upd
+
+        # cv2.waitKey(0)
+
+    # cv2.imshow('Result', img)
+    # cv2.waitKey(0)
+
+    return img
+
+def process(img, verbose):
     # cv2.namedWindow('collect coordinate')
     # cv2.setMouseCallback('collect coordinate', print_position)
     # cv2.imshow('collect coordinate', img)
@@ -21,7 +212,7 @@ def process(img):
     #         break
 
     w, h = img.shape[:2]
-    corners = np.float32([[8, 15], [233, 5], [30, 235], [249, 227]])
+    corners = np.float32([[9, 15], [233, 5], [30, 235], [249, 227]])
     bounds = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
 
     M = cv2.getPerspectiveTransform(corners, bounds)
@@ -40,11 +231,17 @@ def process(img):
 
     kernel = np.ones((3,3),np.uint8)
     mask = cv2.dilate(mask, kernel, iterations = 4)
+    # bilat2 = bilat.copy()
+    # bilat2 = cv2.GaussianBlur(bilat, (3, 3), 2)
+    # mask = cv2.dilate(mask, kernel, iterations = 2)
+    # bilat[mask == 255] = bilat2[mask == 255]
 
-    paint = cv2.inpaint(bilat,mask,5,cv2.INPAINT_NS)
+    paint = exemplar_paint(bilat)
+
+    # paint = cv2.inpaint(bilat,mask,5,cv2.INPAINT_NS)
 
     paint2 = paint.copy()
-    paint2 = cv2.medianBlur(paint, 7)
+    paint2 = cv2.GaussianBlur(paint, (3, 3), 2)
     mask = cv2.dilate(mask, kernel, iterations = 2)
     paint[mask == 255] = paint2[mask == 255]
 
@@ -139,32 +336,37 @@ def process(img):
     # lab = cv2.merge([l, a, b])
     # contrast2 = cv2.cvtColor(lab, cv2.COLOR_Lab2BGR)
 
-    # cv2.imshow('orig', img)
-    # cv2.imshow('warp', warp)
-    # cv2.imshow('denoise', denoise)
-    # cv2.imshow('bilat', bilat)
-    # # cv2.imshow('median', median)
-    # cv2.imshow('paint', paint)
-    # # cv2.imshow('median2', median2)
-    # cv2.imshow('sharp', sharp)
-    # cv2.imshow('contrast', contrast)
-    # cv2.imshow('denoise2', denoise2)
-    # # cv2.imshow('bilat2', bilat2)
-    # # cv2.imshow('denoise3', denoise3)
-    # cv2.imshow('gamma', gamma)
-    # # cv2.imshow('contrast2', contrast2)
-    # cv2.imshow('col', col)
-    # cv2.imshow('l', l)
-    # cv2.imshow('s', s)
-    # cv2.imshow('v', v)
-    # cv2.imshow('h', h)
-    # cv2.waitKey(0)
+    if verbose:
+        cv2.imshow('orig', img)
+        cv2.imshow('warp', warp)
+        cv2.imshow('denoise', denoise)
+        cv2.imshow('bilat', bilat)
+        # cv2.imshow('median', median)
+        cv2.imshow('paint', paint)
+        # cv2.imshow('median2', median2)
+        cv2.imshow('sharp', sharp)
+        cv2.imshow('contrast', contrast)
+        cv2.imshow('denoise2', denoise2)
+        # cv2.imshow('bilat2', bilat2)
+        # cv2.imshow('denoise3', denoise3)
+        # cv2.imshow('gamma', gamma)
+        # cv2.imshow('contrast2', contrast2)
+        # cv2.imshow('col', col)
+        # cv2.imshow('l', l)
+        # cv2.imshow('s', s)
+        # cv2.imshow('v', v)
+        # cv2.imshow('h', h)
+        cv2.waitKey(0)
     return denoise2
 
 if __name__ == "__main__":
     path = sys.argv[1]
     if not os.path.exists("./Results"):
         os.makedirs("./Results")
+    
+    verbose = False
+    if len(sys.argv) == 3:
+        verbose = True
 
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -175,6 +377,6 @@ if __name__ == "__main__":
 
             img = cv2.imread(file_path, cv2.IMREAD_COLOR)
 
-            res = process(img)
+            res = process(img, verbose)
 
             cv2.imwrite(os.path.join("./Results", file), res)
